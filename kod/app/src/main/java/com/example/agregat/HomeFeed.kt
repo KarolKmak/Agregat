4394bcae6e007f7d950a8b11af4fcf8a44781023
KarolKmak <58179517+KarolKmak@users.noreply.github.com>
2022-04-20 11:49:17 +0200

    Delete kod directory

--- kod/app/src/main/java/com/example/agregat/HomeFeed.kt
+++
@@ -1,7 +0,0 @@
-package com.example.agregat
-
-class HomeFeed(val Adress: String, val Title: String, val article_description: String, val Importance: Int){
-}
-class AgregatFeed(val Name: String,val Description: String){
-}
-
